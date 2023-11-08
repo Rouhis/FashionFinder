a585214f99ebfc0c2967482721e3313867643dd6
rouhis <eeturouhiainen1@gmail.com>
2023-11-08 12:19:38 +0200

    working react bard project added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const styles = {
+  container: {
+    backgroundColor: 'black', // Main background color
+    color: '#8fce00',          // Main text color
+    fontFamily: 'Arial, sans-serif', // Font family
+    minHeight: '100vh',       // Full height view
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    backgroundColor: '#8fce00', // Button background color
+    color: 'black',            // Button text color
+    border: 'none',
+    padding: '10px 20px',
+    fontSize: '1em',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    margin: '10px',
+    borderRadius: '5px',
+    // Disable button style
+    ':disabled': {
+      backgroundColor: 'grey',
+      cursor: 'not-allowed',
+    },
+  },
+  answer: {
+    backgroundColor: '#c9f26c',
+    color: 'black',
+    fontSize: '1em',
+    fontWeight: 'bold',
+    border: '1px solid #8fce00',    // Green border
+    borderRadius: '5px',
+    padding: '20px',
+    marginTop: '20px',
+  }
+};
+
+
+const App = () => {
+  const [answer, setAnswer] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null); // New state for the image
+
+  const handleImageChange = (e) => {
+    setSelectedImage(e.target.files[0]);
+  };
+
+  const handleAskBard = async () => {
+    setIsLoading(true);
+
+    const formData = new FormData();
+    formData.append('image', selectedImage); // Append the image to the FormData object
+    formData.append('question', 'Find me products similiar to this from zalando and return the answer in JSON format.'); // Append other data you want to send
+
+    try {
+      const response = await axios.post('http://localhost:3001/ask-bard', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data', // Important header for files
+        },
+      });
+      setAnswer(response.data.answer);
+    } catch (error) {
+      console.error('Error fetching response from Bard:', error);
+    }
+
+    setIsLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.container}>
+      <input 
+        type="file" 
+        onChange={handleImageChange} 
+        disabled={isLoading} 
+      />
+      <button 
+        onClick={handleAskBard} 
+        disabled={isLoading || !selectedImage} // Disable button if loading or no image is selected
+        style={isLoading ? { ...styles.button, ...styles.button[':disabled'] } : styles.button}
+      >
+        {isLoading ? 'Loading...' : 'Ask Bard'}
+      </button>
+      <div style={styles.answer}>Answer from Bard: {answer}</div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+const cors = require('cors');
+const multer = require('multer'); // Import multer
+
+app.use(cors());
+
+// Set up multer for file storage
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(express.json());
+
+const bardCookie = 'dAi4m1_QwJyS1clHbsMGcMr4Pi60Mp8EPVRI-t5K7y0TcWDP3hBPL90QdbF7S5zNW7DFcA.'; // Replace with your actual Bard AI cookie securely
+
+app.post('/ask-bard', upload.single('image'), async (req, res) => {
+    const { question } = req.body;
+    const image = req.file.buffer; // The image file will be available here
+    // You can now use the image buffer and question to interact with your Bard AI or any other service
+    try {
+        // For example, if your Bard AI can process images:
+         const Bard = (await import('bard-ai')).default;
+         const myBard = new Bard(bardCookie);
+         const answer = await myBard.ask(question, {image: image.buffer});
+        console.log(answer);
+        res.json({ answer });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
