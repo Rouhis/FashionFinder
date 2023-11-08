03cf48ddc972dbeafa3436a855b8474a9f89d997
AxelKah <axel.kahkonen@gmail.com>
2023-11-08 12:56:09 +0200

    Bard now returns answer in "json" format in box

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import * as React from 'react';
+import React, {useState} from 'react';
 import Button from '@mui/joy/Button';
 import { Box } from '@mui/system';
 import LoadPic from './load_pic.png'
@@ -9,12 +9,38 @@ import Card from '@mui/joy/Card';
 import CardContent from '@mui/joy/CardContent';
 import Typography from '@mui/joy/Typography';
 import testjacket from './jacket.png';
+import axios from 'axios';
 
 
-function App() {
-  const ClickHandeler = () =>{
-    alert("Button clicked")
-  }
+const App = () => {
+    const [answer, setAnswer] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null); // New state for the image
+  
+    const handleImageChange = (e) => {
+      setSelectedImage(e.target.files[0]);
+    };
+  
+    const handleAskBard = async () => {
+      setIsLoading(true);
+  
+      const formData = new FormData();
+      formData.append('image', selectedImage); // Append the image to the FormData object
+      formData.append('question', 'Find me products similiar to this from zalando and return the answer in JSON format.'); // Append other data you want to send
+  
+      try {
+        const response = await axios.post('http://localhost:3001/ask-bard', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data', // Important header for files
+          },
+        });
+        setAnswer(response.data.answer);
+      } catch (error) {
+        console.error('Error fetching response from Bard:', error);
+      }
+  
+      setIsLoading(false);
+    };
 
   return (
     <div className="App"> 
@@ -25,12 +51,15 @@ function App() {
             <div className="HeaderBox">
               <h5 className="BoxTitle">Load Image</h5>
             </div>
-            <img src={LoadPic} alt="Logo"></img>
-          </Box>
+            <input 
+        type="file" 
+        onChange={handleImageChange} 
+        disabled={isLoading} 
+      />
+        </Box>
           <div className="ConfirmButtonBox">
-            <Button color="neutral" className="ConfirmButton" variant="solid">Confirm Selection</Button>
+            <Button color="neutral" className="ConfirmButton" variant="solid" onClick={handleAskBard} disabled={isLoading || !selectedImage}>Confirm Selection</Button>
           </div>
-
           <Box className="PreviewBox">
         <div className="HeaderBox">
               <h5 className="BoxTitle">Preview</h5>
@@ -44,9 +73,7 @@ function App() {
               <h5 className="BoxTitle">Similar Products Information</h5>
             </div>
               <p className="InfoText">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. In tellus integer feugiat scelerisque varius morbi enim. Et magnis dis parturient montes nascetur ridiculus. In nibh mauris cursus mattis molestie a iaculis. Condimentum mattis pellentesque id nibh tortor. Adipiscing tristique risus nec feugiat in fermentum posuere. Vitae turpis massa sed elementum tempus. A diam sollicitudin tempor id eu nisl nunc. Phasellus vestibulum lorem sed risus. Ipsum dolor sit amet consectetur. Dui id ornare arcu odio ut sem nulla pharetra diam. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Lacus sed turpis tincidunt id aliquet risus. Blandit aliquam etiam erat velit scelerisque in dictum non consectetur. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Nibh venenatis cras sed felis eget velit aliquet sagittis id. Ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Lorem donec massa sapien faucibus et molestie.
-                Lectus magna fringilla urna porttitor. Orci ac auctor augue mauris augue neque gravida in fermentum. Vivamus arcu felis bibendum ut tristique. Donec et odio pellentesque diam volutpat commodo sed egestas. Urna condimentum mattis pellentesque id nibh. Platea dictumst quisque sagittis purus. Nibh nisl condimentum id venenatis. Senectus et netus et malesuada fames ac turpis egestas. Porta nibh venenatis cras sed. Dignissim sodales ut eu sem integer vitae. Posuere ac ut consequat semper viverra nam libero justo. Vestibulum rhoncus est pellentesque elit. Diam vel quam elementum pulvinar etiam. Odio pellentesque diam volutpat commodo sed egestas egestas. Tristique senectus et netus et malesuada fames ac turpis. Purus ut faucibus pulvinar elementum integer enim neque volutpat ac
-                Lectus magna fringilla urna porttitor. Orci ac auctor augue mauris augue neque gravida in fermentum. Vivamus arcu felis bibendum ut tristique. Donec et odio pellentesque diam volutpat commodo sed egestas. Urna condimentum mattis pellentesque id nibh. Platea dictumst quisque sagittis purus. Nibh nisl condimentum id venenatis. Senectus et netus et malesuada fames ac turpis egestas. Porta nibh venenatis cras sed. Dignissim sodales ut eu sem integer vitae. Posuere ac ut consequat semper viverra nam libero justo. Vestibulum rhoncus e
+                {answer}
               </p>
             </Box>
         </div>
